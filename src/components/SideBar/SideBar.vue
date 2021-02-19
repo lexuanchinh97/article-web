dfc239e7578d8d2ee805bc3673164a9fbefb7a6c
liam <lexuanchinh1507@gmail.com>
2021-02-20 00:24:13 +0700

    side bar

--- src/components/SideBar/SideBar.vue
+++ src/components/SideBar/SideBar.vue
@@ -1,6 +1,263 @@
 <template>
-  <div class="test">
-      <h1>test</h1>
+  <div class="row side-bar">
+    <div class="row new">
+      <div class="row">
+        <h3>Tin Mới</h3>
+      </div>
+      <div class="row container">
+        <div class="col-4">
+          <a class="imghover" href="">
+            <figure>
+              <img
+                src="http://localhost:8888/wp-content/uploads/2018/11/hvgh-gh-g-720x540.jpg"
+                alt="hihi"
+                style="width: 100%"
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="col-8">
+          <a href="">
+            <h4>
+              Tuyển Việt Nam đấu tập trung bảng G, gặp Malaysia vào tháng 6
+            </h4>
+          </a>
+        </div>
+      </div>
+
+      <div class="row container">
+        <div class="col-4">
+          <a class="imghover" href="">
+            <figure>
+              <img
+                src="http://localhost:8888/wp-content/uploads/2018/11/hvgh-gh-g-720x540.jpg"
+                alt="hihi"
+                style="width: 100%"
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="col-8">
+          <a href="">
+            <h4>
+              Tuyển Việt Nam đấu tập trung bảng G, gặp Malaysia vào tháng 6
+            </h4>
+          </a>
+        </div>
+      </div>
+
+      <div class="row container">
+        <div class="col-4">
+          <a class="imghover" href="">
+            <figure>
+              <img
+                src="http://localhost:8888/wp-content/uploads/2018/11/hvgh-gh-g-720x540.jpg"
+                alt="hihi"
+                style="width: 100%"
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="col-8">
+          <a href="">
+            <h4>
+              Tuyển Việt Nam đấu tập trung bảng G, gặp Malaysia vào tháng 6
+            </h4>
+          </a>
+        </div>
+      </div>
+
+      <div class="row container">
+        <div class="col-4">
+          <a class="imghover" href="">
+            <figure>
+              <img
+                src="http://localhost:8888/wp-content/uploads/2018/11/hvgh-gh-g-720x540.jpg"
+                alt="hihi"
+                style="width: 100%"
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="col-8">
+          <a href="">
+            <h4>
+              Tuyển Việt Nam đấu tập trung bảng G, gặp Malaysia vào tháng 6
+            </h4>
+          </a>
+        </div>
+      </div>
+
+      <div class="row container">
+        <div class="col-4">
+          <a class="imghover" href="">
+            <figure>
+              <img
+                src="http://localhost:8888/wp-content/uploads/2018/11/hvgh-gh-g-720x540.jpg"
+                alt="hihi"
+                style="width: 100%"
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="col-8">
+          <a href="">
+            <h4>
+              Tuyển Việt Nam đấu tập trung bảng G, gặp Malaysia vào tháng 6
+            </h4>
+          </a>
+        </div>
+      </div>
+
+      <div class="row container">
+        <div class="col-4">
+          <a class="imghover" href="">
+            <figure>
+              <img
+                src="http://localhost:8888/wp-content/uploads/2018/11/hvgh-gh-g-720x540.jpg"
+                alt="hihi"
+                style="width: 100%"
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="col-8">
+          <a href="">
+            <h4>
+              Tuyển Việt Nam đấu tập trung bảng G, gặp Malaysia vào tháng 6
+            </h4>
+          </a>
+        </div>
+      </div>
+
+      <div class="row container">
+        <div class="col-4">
+          <a class="imghover" href="">
+            <figure>
+              <img
+                src="http://localhost:8888/wp-content/uploads/2018/11/hvgh-gh-g-720x540.jpg"
+                alt="hihi"
+                style="width: 100%"
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="col-8">
+          <a href="">
+            <h4>
+              Tuyển Việt Nam đấu tập trung bảng G, gặp Malaysia vào tháng 6
+            </h4>
+          </a>
+        </div>
+      </div>
+
+      <div class="row container">
+        <div class="col-4">
+          <a class="imghover" href="">
+            <figure>
+              <img
+                src="http://localhost:8888/wp-content/uploads/2018/11/hvgh-gh-g-720x540.jpg"
+                alt="hihi"
+                style="width: 100%"
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="col-8">
+          <a href="">
+            <h4>
+              Tuyển Việt Nam đấu tập trung bảng G, gặp Malaysia vào tháng 6
+            </h4>
+          </a>
+        </div>
+      </div>
+
+      <div class="row container">
+        <div class="col-4">
+          <a class="imghover" href="">
+            <figure>
+              <img
+                src="http://localhost:8888/wp-content/uploads/2018/11/hvgh-gh-g-720x540.jpg"
+                alt="hihi"
+                style="width: 100%"
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="col-8">
+          <a href="">
+            <h4>
+              Tuyển Việt Nam đấu tập trung bảng G, gặp Malaysia vào tháng 6
+            </h4>
+          </a>
+        </div>
+      </div>
+
+      <div class="row container">
+        <div class="col-4">
+          <a class="imghover" href="">
+            <figure>
+              <img
+                src="http://localhost:8888/wp-content/uploads/2018/11/hvgh-gh-g-720x540.jpg"
+                alt="hihi"
+                style="width: 100%"
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="col-8">
+          <a href="">
+            <h4>
+              Tuyển Việt Nam đấu tập trung bảng G, gặp Malaysia vào tháng 6
+            </h4>
+          </a>
+        </div>
+      </div>
+
+      <div class="row container">
+        <div class="col-4">
+          <a class="imghover" href="">
+            <figure>
+              <img
+                src="http://localhost:8888/wp-content/uploads/2018/11/hvgh-gh-g-720x540.jpg"
+                alt="hihi"
+                style="width: 100%"
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="col-8">
+          <a href="">
+            <h4>
+              Tuyển Việt Nam đấu tập trung bảng G, gặp Malaysia vào tháng 6
+            </h4>
+          </a>
+        </div>
+      </div>
+
+      <div class="row container">
+        <div class="col-4">
+          <a class="imghover" href="">
+            <figure>
+              <img
+                src="http://localhost:8888/wp-content/uploads/2018/11/hvgh-gh-g-720x540.jpg"
+                alt="hihi"
+                style="width: 100%"
+              />
+            </figure>
+          </a>
+        </div>
+        <div class="col-8">
+          <a href="">
+            <h4>
+              Tuyển Việt Nam đấu tập trung bảng G, gặp Malaysia vào tháng 6
+            </h4>
+          </a>
+        </div>
+      </div>
+    </div>
+
+
   </div>
 </template>
 
@@ -11,9 +268,65 @@ export default {
 </script>
 
 <style>
-    .test {
-        background-color: red;
-        height: 100px;
-        
-    }
+figure {
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  background: #fff;
+  overflow: hidden;
+}
+
+.imghover figure img {
+  -webkit-transform: scale(1);
+  transform: scale(1);
+  -webkit-transition: 0.3s ease-in-out;
+  transition: 0.3s ease-in-out;
+  vertical-align:bottom;
+}
+
+.imghover figure:hover img {
+  -webkit-transform: scale(1.05);
+  transform: scale(1.05);
+}
+.side-bar {
+  padding: 8px 8px;
+  /*background: red;*/
+}
+
+.side-bar .new {
+  background: #f5f5f5;
+}
+
+.side-bar h3 {
+  margin: 0 0;
+  text-align: center;
+  padding: 8px 8px;
+}
+
+.side-bar h4 {
+  margin: 0 0;
+  text-align: left;
+  padding: 8px 8px;
+}
+.side-bar a {
+  text-decoration: none;
+  color: #333333;
+  font-size: 14px;
+  text-align: left;
+}
+
+.row h4:hover {
+  color: #0082e6;
+}
+
+.side-bar .container {
+  padding: 4px 8px;
+}
+
+@media only screen and (max-width: 600px) {
+  .side-bar {
+   display: none;
+  }
+
+}
 </style>
